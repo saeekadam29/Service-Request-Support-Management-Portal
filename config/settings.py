acb72f437b1f144d27fa55997b5cd63f0461e9e3
Saee kadam <saeekadam2909@gmail.com>
2026-09-13 15:27:28 +0530

    Add live Render URL to allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY', default='dev-only-insecure-secret-key-change-m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,service-request-support-management-portal.onrender.com', cast=Csv())
 
 INSTALLED_APPS = [
     'django.contrib.admin',
